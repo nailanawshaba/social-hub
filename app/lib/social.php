4bc180a971aef9634ac7de8ca8de989ed8832521
Álvaro Moraleda Mohedano <alvaro.moraleda@gmail.com>
2014-03-18 21:27:22 +0100

    Bug solved: now Twitter id is passed

--- app/lib/social.php
+++ app/lib/social.php
@@ -1,7 +1,7 @@
 <?php
 	require_once __DIR__ . "/php/socialhub/CustomPostsFactory.class.php";
 	$nextFacebook = $_POST['nextFacebook'];
-	$nextTwitter = $POST["nextTwitter"];
+	$nextTwitter = $_POST["nextTwitter"];
 	$postFactory = CustomPostsFactory::getSocialHub(array("Facebook", "Twitter"), array($nextFacebook,$nextTwitter));
 
 	echo json_encode($postFactory);
